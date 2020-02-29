71034d989167017c2743f90e97dc1b6229761f80
Delusive- <zoncck@yandex.ru>
2020-02-29 14:32:34 +0300

    Define some beans and create config file

--- src/main/java/space/delusive/telegram/config/SpringConfiguration.java
+++ src/main/java/space/delusive/telegram/config/SpringConfiguration.java
@@ -1,9 +1,28 @@
 package space.delusive.telegram.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Import(WebMvcConfiguration.class)
+@PropertySource("classpath:application.properties")
 public class SpringConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public String sendMessageUrl(@Value("${bot.url.send.message}") String sendMessageUrl) {
+        return sendMessageUrl;
+    }
+
+    @Bean
+    public String botToken(@Value("${bot.token}") String botToken) {
+        return botToken;
+    }
 }
